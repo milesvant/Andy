ea40569c95c1e8241adb53f8e1c13274b49bd2ca
Miles Van Tongeren <mnv2108@columbia.edu>
2018-07-11 13:40:05 -0700

    Added support for asking for tomorrow/this week's events

--- modules/andy_calendar/andy_calendar.py
+++ modules/andy_calendar/andy_calendar.py
@@ -58,6 +58,45 @@ class Calendar:
         events = events_result.get('items', [])
         return events
 
+    def get_tomorrow_events(self):
+        """Returns a list of the events on the day after the current day."""
+        today = datetime.utcnow().date()
+        start = datetime(today.year, today.month, today.day) + timedelta(1)
+        end = start + timedelta(1)
+        # Put dates in correct format for the API call
+        start = (start.isoformat()) + 'Z'
+        end = (end.isoformat()) + 'Z'
+
+        events_result = self.calendar.events().list(
+            calendarId='primary',
+            timeMin=start,
+            timeMax=end,
+            singleEvents=True,
+            orderBy='startTime',
+        ).execute()
+        events = events_result.get('items', [])
+        return events
+
+    def get_this_week_events(self):
+        """Returns a list of the events on the week containing the current
+            day."""
+        today = datetime.utcnow().date()
+        start_of_week = datetime(today.year, today.month, today.day)
+        end_of_week = start_of_week + timedelta(6)
+        # Put dates in correct format for the API call
+        start_of_week = (start_of_week.isoformat()) + 'Z'
+        end_of_week = (end_of_week.isoformat()) + 'Z'
+
+        events_result = self.calendar.events().list(
+            calendarId='primary',
+            timeMin=start_of_week,
+            timeMax=end_of_week,
+            singleEvents=True,
+            orderBy='startTime',
+        ).execute()
+        events = events_result.get('items', [])
+        return events
+
     def route_command(self, command, say, listen):
         """Executes and generates a string response for a given Calendar
             command.
@@ -74,21 +113,63 @@ class Calendar:
                     false if the command was invalid.
         """
         label, args = self.helper.parse_command(command)
+        # list today's events
         if label == "today events":
             events = self.get_today_events()
             if not events:
-                say("No upcoming events found.")
+                say("No upcoming events today found.")
             else:
-                response = ""
+                response = "Today's events:\n"
                 for event in events:
-                    start = self.helper.get_time(event['start'].get(
-                        'dateTime',
-                        event['start'].get('date')))
-                    end = self.helper.get_time(event['end'].get(
+                    start = self.helper.get_non_military_time(
+                        event['start'].get(
+                            'dateTime',
+                            event['start'].get('date')))
+                    end = self.helper.get_non_military_time(event['end'].get(
                         'dateTime',
                         event['end'].get('date')))
                     response += "From " + start + " to " + end + " - "
                     response += event['summary'] + "\n"
+                say(response)
+        # list tomorrow's events
+        elif label == "tomorrow events":
+            events = self.get_tomorrow_events()
+            if not events:
+                say("No events found tomorrow.")
+            else:
+                response = "Tommorow's events:\n"
+                for event in events:
+                    start = self.helper.get_non_military_time(
+                        event['start'].get(
+                            'dateTime',
+                            event['start'].get('date')))
+                    end = self.helper.get_non_military_time(event['end'].get(
+                        'dateTime',
+                        event['end'].get('date')))
+                    response += "From " + start + " to " + end + " - "
+                    response += event['summary'] + "\n"
+                say(response)
+        # list this week's events
+        elif label == "current week events":
+            events = self.get_this_week_events()
+            if not events:
+                say("No events found this week.")
+            else:
+                response = "This week's events:\n"
+                for event in events:
+                    day_of_week = self.helper.get_day_of_week_from_string(
+                        event['start'].get(
+                            'dateTime',
+                            event['start'].get('date')))
+                    start = self.helper.get_non_military_time(
+                        event['start'].get(
+                            'dateTime',
+                            event['start'].get('date')))
+                    end = self.helper.get_non_military_time(event['end'].get(
+                        'dateTime',
+                        event['end'].get('date')))
+                    response += "On " + day_of_week + " from " + start + " to "
+                    response += end + " - " + event['summary'] + "\n"
                 say(response)
         else:
             return False
--- modules/andy_calendar/calendar_helper.py
+++ modules/andy_calendar/calendar_helper.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 from ..module_helper import ModuleHelper
 
 
@@ -9,12 +10,39 @@ class CalendarHelper(ModuleHelper):
             calendar_re: a dictionary from string labels to lists of regular
                 expressions which match commands with similar meanings (e.g.
                 'schedule today' and 'What are my calendar events today?')
+            int_to_weekday: a dictionary from ints to the corresponding string
+                of the day of the week (where Monday is 0 and Sunday is 6)
+            weekeday_to_int: a dictionary from string names of the days of the
+                week to their corresponding index in the week
+                (i.e. 0 -> monday)
     """
 
     def __init__(self):
         ModuleHelper.__init__(self)
         self.calendar_re = {
             "today events": [re.compile('[A-Z|a-z| |\']*(calendar|schedule|events)[A-Z|a-z| |\']*today\??'), ],
+            "tomorrow events": [re.compile('[A-Z|a-z| |\']*(calendar|schedule|events)[A-Z|a-z| |\']*tomorrow\??'), ],
+            "current week events": [re.compile('[A-Z|a-z| |\']*(calendar|schedule|events)[A-Z|a-z| |\']*(this|next) week\??'), ],
+        }
+
+        self.int_to_weekday = {
+            0: "Monday",
+            1: "Tuesday",
+            2: "Wednesday",
+            3: "Thursday",
+            4: "Friday",
+            5: "Saturday",
+            6: "Sunday",
+        }
+
+        self.weekday_to_int = {
+            "monday": 0,
+            "tuesday": 1,
+            "wednesday": 2,
+            "thursday": 3,
+            "friday": 4,
+            "saturday": 5,
+            "sunday": 6,
         }
 
     def parse_command(self, command):
@@ -35,7 +63,7 @@ class CalendarHelper(ModuleHelper):
                     return label, None
         return None, None
 
-    def get_time(self, time_string):
+    def get_non_military_time(self, time_string):
         """Removes dates and military time from strings returned by the Google
             Calendar API.
 
@@ -44,8 +72,23 @@ class CalendarHelper(ModuleHelper):
         """
         after_t = time_string.split('T')[1]
         time = after_t[0:5]
+        # convert from military time and add AM/PM
         if int(time[0:2]) > 12:
             time = str(int(time[0:2]) - 12) + time[2:5] + " PM"
         else:
             time += " AM"
         return time
+
+    def get_day_of_week_from_string(self, time_string):
+        """Converts a string returned from a Google Calendar API call to a
+            day of the week.
+
+            Example:
+                2018-07-11T16:30:00-04:00 -> Wednesday
+        """
+        year = time_string[0:4]
+        month = time_string[5:7]
+        day = time_string[8:10]
+        date = datetime(int(year), int(month), int(day))
+        day_of_week = date.weekday()
+        return self.int_to_weekday[day_of_week]
